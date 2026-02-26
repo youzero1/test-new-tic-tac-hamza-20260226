7feeaa050ba9566a9df1eb4012a049ba10b43891
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-02-26 19:08:12 +0500

    Add lib/entities/Product.ts

---
+++ lib/entities/Product.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column("text")
+    description: string
+
+    @Column("decimal")
+    price: number
+
+    @Column()
+    imageUrl: string
+}
