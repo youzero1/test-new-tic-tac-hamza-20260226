edb2f8befc6fbd0d336a93475c7a91d24642058b
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-02-26 19:08:07 +0500

    Add pages/api/cart.ts

---
+++ pages/api/cart.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        res.status(200).json({ message: 'Added to cart' })
+    } else {
+        res.status(405).json({ message: 'Method not allowed' })
+    }
+}
